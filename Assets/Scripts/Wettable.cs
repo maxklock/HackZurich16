7c972014093182c69e6cef0907d32a44dfba7d18
Anke <anke@isg.cs.uni-magdeburg.de>
2016-09-18 00:45:03 +0200

    Wetting not only your Bed!

---
+++ Assets/Scripts/Wettable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(InsuredObject))]
+public class Wettable : MonoBehaviour {
+    public ParticleSystem Drops;
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void MakeWet(MakeWet water)
+    {
+        Drops.gameObject.SetActive(true);
+        gameObject.GetComponent<InsuredObject>().SetDamage(InsuredObject.ObjectDamage.TabWater);
+    }
+}
