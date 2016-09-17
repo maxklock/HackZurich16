9fbb7b5d364bc8f699b45377bdf05ca3b151e60d
Anke <anke@isg.cs.uni-magdeburg.de>
2016-09-17 15:55:43 +0200

    Insurances, started with fire

---
+++ Assets/Scripts/BeFired.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeFired : MonoBehaviour {
+    public ParticleSystem Fire;
+	// Use this for initialization
+	void Start () {
+        Fire.Pause();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    //void OnCollisionStart(Collision coll)
+    //{
+    //    //if(coll.gameObject.GetComponent<>)
+    //}
+
+    public void SetOnFire(SetFire fire)
+    {
+        Fire.Play();
+    }
+}
---
+++ Assets/Scripts/SetFire.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetFire : MonoBehaviour {
+    public ParticleSystem Fire;
+
+    // Use this for initialization
+    void Start () {
+        Fire.Pause();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionStart(Collision coll)
+    {
+        BeFired fuel = coll.gameObject.GetComponent<BeFired>();
+        if (fuel != null)
+        {
+            fuel.SetOnFire(this);
+        }
+        Fire.Play();
+    }
+}
