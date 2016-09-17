ccf9078aba7cbe642de6c94b363c69c1951d1e02
Anke <anke@isg.cs.uni-magdeburg.de>
2016-09-17 21:00:10 +0200

    Some breakable objects, windows, fire fix, dirt start

---
+++ Assets/Scripts/EnvironmentSnow.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnvironmentSnow : MonoBehaviour {
+    public bool Active;
+
+	// Use this for initialization
+	void Start () {
+        Active = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnSelected()
+    {
+        Animator controll = GetComponent<Animator>();
+        if(controll != null)
+        {
+            if (!Active)
+            {
+                controll.Play("Move");
+                Active = true;
+                //Start a Storm
+            }
+        }
+    }
+    
+}
